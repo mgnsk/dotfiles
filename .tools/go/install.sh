ed2dd7d390d3a80a009affd1720e4e64cd17a1b1
Magnus Kokk <magnus@kokk.eu>
2020-09-28 18:13:20 +0300

    Skip golangci-lint on arm processors

--- .tools/go/install.sh
+++ .tools/go/install.sh
@@ -5,5 +5,11 @@ set -euo pipefail
 go install $(awk '/import\s\(/{flag=1;next}/)/{flag=0}flag' tools.go | cut -d \" -f2)
 go clean -modcache
 
+machine=$(uname -m)
+if [[ "$machine" == 'armv8l' ]]; then
+	echo "### Skipping golangci-lint"
+	exit 0
+fi
+
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.31.0
 golangci-lint --version

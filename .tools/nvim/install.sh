7327a443d22cdb1a5d2bc2409439e2f7d9ca9bc4
Magnus Kokk <magnus@kokk.eu>
2020-11-24 14:00:12 +0200

    Install neovim with tools

--- .tools/nvim/install.sh
+++ .tools/nvim/install.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+NEOVIM_COMMIT=71d4f5851f068eeb432af34850dddda8cc1c71e3
+
+src_dir=~/neovim_src
+if [ -d $src_dir ]; then
+	cd $src_dir
+	git checkout master
+	git pull
+else
+	git clone https://github.com/neovim/neovim.git $src_dir
+fi
+
+cd $src_dir
+git checkout $NEOVIM_COMMIT
+
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.neovim -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+make install
+make distclean
+
+nvim --version
+
 if test ! -f ~/.vim/autoload/plug.vim; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

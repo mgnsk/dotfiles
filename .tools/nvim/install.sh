1a17cd6054b79ff90e9fc663ef3996d0675b896f
Magnus Kokk <magnus@kokk.eu>
2020-11-24 15:34:42 +0200

    clean up

--- .tools/nvim/install.sh
+++ .tools/nvim/install.sh
@@ -2,37 +2,30 @@
 
 set -e
 
-NEOVIM_COMMIT=71d4f5851f068eeb432af34850dddda8cc1c71e3
+neovim_commit=71d4f5851f068eeb432af34850dddda8cc1c71e3
 
-src_dir=~/neovim_src
-if [ -d $src_dir ]; then
-	cd $src_dir
-	git checkout master
-	git pull
-else
-	git clone https://github.com/neovim/neovim.git $src_dir
-fi
-
-cd $src_dir
-git checkout $NEOVIM_COMMIT
-
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.neovim -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+wget https://github.com/neovim/neovim/archive/${neovim_commit}.zip
+unzip -q ${neovim_commit}.zip
+rm ${neovim_commit}.zip
+cd neovim-${neovim_commit}
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.neovim -DCMAKE_BUILD_TYPE=Release"
 make install
-make distclean
+cd ..
+rm -rf neovim-${neovim_commit}
 
 nvim --version
 
 if test ! -f ~/.vim/autoload/plug.vim; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if test ! -f ~/.vim/init.vim; then
-    ln -s "$HOME/.vimrc" "$HOME/.vim/init.vim"
+	ln -s "$HOME/.vimrc" "$HOME/.vim/init.vim"
 fi
 
 if test ! -d ~/.config/nvim; then
-    ln -s "$HOME/.vim" "$HOME/.config/nvim"
+	ln -s "$HOME/.vim" "$HOME/.config/nvim"
 fi
 
 nvim --headless -u ~/.vim/plugins.vim -S ~/.vim/plugin.lock -c 'qa'

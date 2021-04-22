eab884f843560dd5497bc9bdfa7b1bfe67be919d
Magnus Kokk <magnus@kokk.eu>
2021-04-22 14:48:04 +0300

    nvim LSP open in new tab

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,3 +1,7 @@
+local api = vim.api
+local util = vim.lsp.util
+local handlers = vim.lsp.handlers
+local log = vim.lsp.log
 local vimp = require "vimp"
 local lsp = require "lspconfig"
 
@@ -12,6 +16,31 @@ lsp.yamlls.setup {}
 lsp.html.setup {}
 lsp.cssls.setup {}
 lsp.bashls.setup {}
+
+local location_callback = function(_, method, result)
+    if result == nil or vim.tbl_isempty(result) then
+        local _ = log.info() and log.info(method, "No location found")
+        return nil
+    end
+
+    api.nvim_command("tab split")
+
+    if vim.tbl_islist(result) then
+        util.jump_to_location(result[1])
+        if #result > 1 then
+            util.set_qflist(util.locations_to_items(result))
+            api.nvim_command("copen")
+            api.nvim_command("wincmd p")
+        end
+    else
+        util.jump_to_location(result)
+    end
+end
+
+handlers["textDocument/declaration"] = location_callback
+handlers["textDocument/definition"] = location_callback
+handlers["textDocument/typeDefinition"] = location_callback
+handlers["textDocument/implementation"] = location_callback
 
 vimp.nnoremap({"silent"}, "<space>a", vim.lsp.diagnostic.set_loclist)
 vimp.nnoremap({"silent"}, "<space>j", vim.lsp.diagnostic.goto_next)

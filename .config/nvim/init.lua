b80da8bd0dfc4764f4b9f22e61503cfe7b2b8b8f
Magnus Kokk <magnus@kokk.eu>
2021-08-02 13:16:55 +0300

    Configure colorizer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,7 +69,14 @@ vim.cmd("set tabline=%!MyTabLine()")
 -- TODO what does this do?
 --vim.cmd("set noruler")
 
-require "colorizer".setup()
+-- Use the `default_options` as the second parameter, which uses
+-- `foreground` for every mode. This is the inverse of the previous
+-- setup configuration.
+require "colorizer".setup {
+    "*", -- Highlight all files, but customize some others.
+    css = {rgb_fn = true}, -- Enable parsing rgb(...) functions in css.
+    html = {names = false} -- Disable parsing "names" like Blue or Gray
+}
 
 vim.g.neomake_open_list = 2
 -- Open files in new tab.

28888f676dde3bc52e3f9ec12459b7b85afb2f8e
Magnus Kokk <magnus@kokk.eu>
2021-08-19 22:55:19 +0300

    clean up tools

--- .tools/go/install.sh
+++ .tools/go/install.sh
@@ -4,19 +4,15 @@ set -e
 
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/golang/mock/mockgen@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/mgechev/revive@latest
-go install github.com/ofabry/go-callvis@latest
 go install github.com/rliebz/tusk@latest
 go install github.com/uber/prototool/cmd/prototool@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/cmd/guru@latest
 go install golang.org/x/tools/gopls@latest
-go install mvdan.cc/gofumpt@latest
 go install mvdan.cc/gofumpt/gofumports@latest
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
-go install github.com/mattn/efm-langserver@latest
 
 cd maildate
 go install .

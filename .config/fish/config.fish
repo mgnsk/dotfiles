4c645b3e1711f7130b00f7cae15d32970b2ba583
Magnus Kokk <magnus@kokk.eu>
2021-09-21 09:34:17 +0300

    Update for ruby 3.0.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ set -gx PATH "$HOME/go/bin:$PATH"
 set -gx PATH "$HOME/.cargo/bin:$PATH"
 set -gx PATH "$HOME/.bin:$PATH"
 set -gx PATH "$HOME/.luarocks/bin:$PATH"
-set -gx PATH "$HOME/.gem/ruby/2.7.0/bin:$PATH"
+set -gx PATH "$HOME/.gem/ruby/3.0.0/bin:$PATH"
 set -gx PATH "/usr/local/bin:$PATH"
 set -gx PATH (printf "%s" "$PATH" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
 

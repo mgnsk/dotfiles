d1137ff04d2a10518c1a0b2d28e6f84909594673
Magnus Kokk <magnus@kokk.eu>
2021-09-23 13:14:05 +0300

    Re-enable golangci-lint

--- .tools/go/install.sh
+++ .tools/go/install.sh
@@ -5,6 +5,7 @@ set -e
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/golang/mock/mockgen@latest
 go install github.com/mgechev/revive@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/rliebz/tusk@latest
 go install github.com/uber/prototool/cmd/prototool@latest
 go install golang.org/x/tools/cmd/godoc@latest

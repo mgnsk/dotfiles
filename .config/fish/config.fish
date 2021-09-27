18cf7077912e243e3825db5ce3b8b885f084af97
Magnus Kokk <magnus@kokk.eu>
2021-09-27 14:37:13 +0300

    Add local go to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx PATH "$HOME/.bin:$PATH"
 set -gx PATH "$HOME/.luarocks/bin:$PATH"
 set -gx PATH "$HOME/.gem/ruby/3.0.0/bin:$PATH"
 set -gx PATH "/usr/local/bin:$PATH"
+set -gx PATH "$HOME/toolbox/workspaces/go/bin:$PATH"
 set -gx PATH (printf "%s" "$PATH" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
 
 set -gx SHELL "/usr/bin/fish"

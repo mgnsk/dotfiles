11e361888f4ff86c7186d62a7fda1a127597f3a1
Magnus Kokk <magnus@kokk.eu>
2021-10-10 18:03:01 +0300

    nvim: configure Tab and CR for autocomplete

--- .config/nvim/lua/autocomplete.lua
+++ .config/nvim/lua/autocomplete.lua
@@ -1,10 +1,20 @@
-require "cmp".setup(
+local cmp = require "cmp"
+cmp.setup(
     {
         sources = {
             {name = "nvim_lsp"},
             {name = "buffer"},
             {name = "nvim_lua"},
             {name = "path"}
+        },
+        mapping = {
+            ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+            ["<CR>"] = cmp.mapping.confirm(
+                {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true
+                }
+            )
         }
     }
 )

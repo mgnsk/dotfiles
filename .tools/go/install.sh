576cf20432ee3b9b70a466a39037fe953942cf05
Magnus Kokk <magnus@kokk.eu>
2021-12-23 18:10:15 +0200

    Install go tools verbose

--- .tools/go/install.sh
+++ .tools/go/install.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install github.com/golang/mock/mockgen@latest
-go install github.com/mgechev/revive@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go install github.com/rliebz/tusk@latest
-go install github.com/uber/prototool/cmd/prototool@latest
-go install golang.org/x/tools/cmd/godoc@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install golang.org/x/tools/cmd/guru@latest
-go install golang.org/x/tools/gopls@latest
-go install mvdan.cc/sh/v3/cmd/shfmt@latest
-go install github.com/mgnsk/templatetool@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v github.com/golang/mock/mockgen@latest
+go install -v github.com/mgechev/revive@latest
+go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install -v github.com/rliebz/tusk@latest
+go install -v github.com/uber/prototool/cmd/prototool@latest
+go install -v golang.org/x/tools/cmd/godoc@latest
+go install -v golang.org/x/tools/cmd/goimports@latest
+go install -v golang.org/x/tools/cmd/guru@latest
+go install -v golang.org/x/tools/gopls@latest
+go install -v mvdan.cc/sh/v3/cmd/shfmt@latest
+go install -v github.com/mgnsk/templatetool@latest
 
 cd tools
-go install ./...
+go install -v ./...

176da4810166dd3f5a3d85bad5995daf36c82586
Magnus Kokk <magnus@kokk.eu>
2022-10-07 20:34:21 +0300

    tools: install gocc

--- .tools/go/install.sh
+++ .tools/go/install.sh
@@ -4,6 +4,7 @@ set -e
 
 go install -v github.com/rliebz/tusk@latest
 go install -v github.com/golang/mock/mockgen@latest
+go install -v github.com/goccmack/gocc@latest
 
 cd tools
 go install -v ./...

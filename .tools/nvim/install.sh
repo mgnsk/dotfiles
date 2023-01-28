5e0f1b63f96f3f2826e16f1c0927035392d403de
Magnus Kokk <magnus@kokk.eu>
2023-01-28 20:11:48 +0200

    refactor toolbox

--- .tools/nvim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-nvim --headless "+Lazy! sync" +qa
-nvim --headless -c "TSInstallSync all" -c "qa"
-nvim --headless -c "TSUpdateSync all" -c "qa"
--- .tools/nvim/remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-nvim --headless -c "TSUninstall all" -c "qa" || true
-
-rm -rf ~/.local/share/nvim/site/pack/pkg/start

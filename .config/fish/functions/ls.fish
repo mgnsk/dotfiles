48c3e63c0c236c839eb253250d298f4b2b318d7b
Magnus Kokk <magnus@kokk.eu>
2023-03-25 11:26:49 +0200

    better fish shell config

--- .config/fish/functions/ls.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function ls --wraps='ls --color=auto' --description 'alias ls=ls --color=auto'
- command ls --color=auto $argv;
-end
--- sway.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
-
-exec sway

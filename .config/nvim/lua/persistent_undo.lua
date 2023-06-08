2b5d3c90dec5eb41a598b3a7cdf430e686fd11a9
Magnus Kokk <magnus@kokk.eu>
2023-06-09 00:12:50 +0300

    nvim: try fix undo sometimes missing bug

--- .config/nvim/lua/persistent_undo.lua
+++ .config/nvim/lua/persistent_undo.lua
@@ -1,9 +1,9 @@
-local undodir = vim.fn.stdpath("state") .. "/undo//"
+local undodir = vim.fn.stdpath("state") .. "/undo/"
 vim.fn.mkdir(undodir, "p")
 vim.o.undodir = undodir
 vim.o.undofile = true
 
-local swapdir = vim.fn.stdpath("state") .. "/swap//"
+local swapdir = vim.fn.stdpath("state") .. "/swap/"
 vim.fn.mkdir(swapdir, "p")
 vim.o.directory = swapdir
 vim.o.swapfile = true

3878e301715a47b766eba618bf398c15f24a6fa4
Magnus Kokk <magnus@kokk.eu>
2023-06-28 21:58:47 +0300

    nvim: configure vsnip mappings for nvim-cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -15,6 +15,16 @@ return {
 		cond = not os.getenv("NVIM_DIFF"),
 		event = { "BufEnter" },
 		config = function()
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			local feedkey = function(key, mode)
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+			end
+
 			local cmp = require("cmp")
 			local compare = cmp.config.compare
 
@@ -35,7 +45,7 @@ return {
 					{ name = "vsnip" },
 					{ name = "buffer" },
 					{ name = "nvim_lua" },
-					{ name = "async_path" },
+					{ name = "async_path", keyword_length = 3 },
 				},
 				sorting = {
 					comparators = {
@@ -51,8 +61,26 @@ return {
 					},
 				},
 				mapping = {
-					["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif vim.fn["vsnip#available"](1) == 1 then
+							feedkey("<Plug>(vsnip-expand-or-jump)", "")
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+							feedkey("<Plug>(vsnip-jump-prev)", "")
+						end
+					end, { "i", "s" }),
+
 					["<CR>"] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Insert,
 						select = false,

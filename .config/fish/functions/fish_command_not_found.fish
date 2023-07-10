86405f7353b7e64a7a52744364e187ac4778fc08
Magnus Kokk <magnus@kokk.eu>
2023-07-10 11:08:39 +0300

    fish: remove custom not found handler

--- .config/fish/functions/fish_command_not_found.fish
+++
@@ -1,7 +0,0 @@
-function fish_command_not_found \
-	--description 'Called by Fish when a command is not found'
-	echo "fish: $argv[1]: command not found..."
-	if type -q podman
-		podman run --rm -it --pull never "localhost/$argv[1]" $argv[1..-1]
-	end
-end

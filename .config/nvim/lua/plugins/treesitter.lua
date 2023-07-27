090114512e1e607fc941f16e7b4165446cebc336
Magnus Kokk <magnus@kokk.eu>
2023-07-27 05:21:13 +0300

    nvim: install tree-sitter-balafon

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,38 @@
+local function install(parsers)
+	if #(vim.api.nvim_list_uis()) > 0 then
+		vim.cmd(string.format("TSUpdate %s", table.concat(parsers, " ")))
+	else
+		for _, parser in ipairs(parsers) do
+			local ok, result = pcall(vim.cmd, string.format("TSUpdateSync %s", parser))
+			if not ok then
+				print(result)
+				os.exit(1)
+			end
+		end
+	end
+end
+
 return {
+	{
+		"mgnsk/tree-sitter-balafon",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = function()
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.balafon = {
+				install_info = {
+					url = "https://github.com/mgnsk/tree-sitter-balafon",
+					files = { "src/parser.c" },
+					branch = "main",
+					generate_requires_npm = true,
+					requires_generate_from_grammar = false,
+				},
+				filetype = "bal",
+			}
+			install({ "balafon" })
+		end,
+	},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufEnter" },
@@ -32,21 +66,7 @@ return {
 				"yaml",
 			}
 
-			if #(vim.api.nvim_list_uis()) > 0 then
-				local ok, result = pcall(vim.cmd, string.format("TSUpdate %s", table.concat(parsers, " ")))
-				if not ok then
-					print(result)
-					os.exit(1)
-				end
-			else
-				for _, parser in ipairs(parsers) do
-					local ok, result = pcall(vim.cmd, string.format("TSUpdateSync %s", parser))
-					if not ok then
-						print(result)
-						os.exit(1)
-					end
-				end
-			end
+			install(parsers)
 		end,
 		config = function()
 			require("nvim-treesitter.configs").setup({

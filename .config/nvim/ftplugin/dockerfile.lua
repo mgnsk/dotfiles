9249e0607f29d947e154977279ef50bb310c1652
Magnus Kokk <magnus@kokk.eu>
2023-09-28 19:25:47 +0300

    nvim: retab Dockerfiles before saving

--- .config/nvim/ftplugin/dockerfile.lua
+++ .config/nvim/ftplugin/dockerfile.lua
@@ -2,3 +2,10 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*Dockerfile",
+	callback = function()
+		vim.cmd("silent! retab")
+	end,
+})

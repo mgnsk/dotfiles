42bcc9b6b6305c0581c565c4c90c8c0cf4107e03
Magnus Kokk <magnus@kokk.eu>
2024-02-12 12:59:32 +0200

    nvim: disable swap and backup, keep undo

--- .config/nvim/lua/persistent_undo.lua
+++ .config/nvim/lua/persistent_undo.lua
@@ -2,8 +2,5 @@ local undodir = vim.fn.stdpath("state") .. "/undo/"
 vim.fn.mkdir(undodir, "p")
 vim.o.undodir = undodir
 vim.o.undofile = true
-
-local swapdir = vim.fn.stdpath("state") .. "/swap/"
-vim.fn.mkdir(swapdir, "p")
-vim.o.directory = swapdir
-vim.o.swapfile = true
+vim.o.swapfile = false
+vim.o.backup = false

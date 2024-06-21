6471880bde30e97029d7fd52243fb0de4b824761
Magnus Kokk <magnus@kokk.eu>
2024-06-21 09:00:47 +0300

    toolbox: update rust config

--- .tools/install-rust.sh
+++ .tools/install-rust.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 rustup update
 rustup default stable
-rustup component add rustfmt rust-src clippy rust-analyzer-preview
+rustup component add rustfmt rust-src clippy rust-analyzer
 
 mkdir -p ~/.config/fish/completions
 rustup completions fish >~/.config/fish/completions/rustup.fish

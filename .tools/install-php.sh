3b9058e64d301fb48f1192db0902e442d5cde791
Magnus Kokk <magnus@kokk.eu>
2024-07-22 23:48:14 +0300

    tools: update composer packages

--- .tools/install-php.sh
+++ .tools/install-php.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-composer install --no-dev --no-cache --no-interaction --optimize-autoloader
+/usr/bin/composer install --no-dev --no-cache --no-interaction --optimize-autoloader
 
 echo "extension=pdo_sqlite" | sudo tee /etc/php/conf.d/01-pdo_sqlite.ini

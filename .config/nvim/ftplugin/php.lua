7336d73c2bfe6c598d527b04d9b8e0927a4e7028
Magnus Kokk <magnus@kokk.eu>
2024-09-07 18:02:34 +0300

    nvim: run pint php formatter when config file detected

--- .config/nvim/ftplugin/php.lua
+++ .config/nvim/ftplugin/php.lua
@@ -2,3 +2,7 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
+
+if vim.fn.filereadable(vim.fn.resolve(vim.fn.getcwd() .. "/pint.json")) then
+	require("conform").formatters_by_ft.php = { "pint" }
+end

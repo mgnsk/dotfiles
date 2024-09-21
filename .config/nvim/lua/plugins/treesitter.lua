721e4fc0433bd871fa55fa07401a632501849036
Magnus Kokk <magnus@kokk.eu>
2024-09-21 11:32:04 +0300

    nvim: treesitter-context: configure max lines for code context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -94,5 +94,10 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		event = { "BufEnter" },
+		config = function()
+			require("treesitter-context").setup({
+				multiline_threshold = 1, -- Maximum number of lines to show for a single context
+			})
+		end,
 	},
 }

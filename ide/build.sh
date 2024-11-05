062f9704fb5d06dbe3d51a0c0ceba351307ea996
Magnus Kokk <magnus@kokk.eu>
2024-11-05 14:24:07 +0200

    ide: use git archive to copy dotfiles

--- ide/build.sh
+++ ide/build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function get-build-context {
-	cd "$HOME" && git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" ls-files | tar Tc -
+	cd "$HOME" && git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" archive --format=tar HEAD
 }
 
 rm -rf dotfiles

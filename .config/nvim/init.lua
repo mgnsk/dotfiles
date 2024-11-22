176bae7f41b33ee4c5913c3f0b6456839df6986d
Magnus Kokk <magnus@kokk.eu>
2024-11-22 22:38:24 +0200

    nvim: update lazy.nvim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,7 @@ vim.cmd("set t_ut=")
 
 vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
+		-- Important to schedule this function for performance.
 		vim.schedule(function()
 			vim.opt.foldmethod = "expr"
 			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
@@ -175,4 +176,28 @@ end, { desc = "Browse current file on Github" })
 
 require("diagnostic")
 require("statusline")
-require("lazy_setup")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+})

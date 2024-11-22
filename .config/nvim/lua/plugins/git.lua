8f5029d9c5a7ceb806e8a8344168ba606183b5e4
Magnus Kokk <magnus@kokk.eu>
2024-11-22 11:24:20 +0200

    nvim: re-add git merge conflict keymap

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,5 +2,13 @@
 return {
 	{
 		"tpope/vim-fugitive",
+		init = function()
+			vim.keymap.set(
+				"n",
+				"<leader>W",
+				":Gw!<CR>",
+				{ desc = "Select the current buffer when resolving git conflicts" }
+			)
+		end,
 	},
 }

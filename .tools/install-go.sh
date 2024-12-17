43e4554e93f1cd2a1ab4ecd477028a71ffcb3a5a
Magnus Kokk <magnus@kokk.eu>
2024-12-17 15:39:37 +0200

    tools: use -trimpath flag when installing go tools

--- .tools/install-go.sh
+++ .tools/install-go.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-go install -v github.com/rliebz/tusk@latest
-go install -v github.com/goccmack/gocc@latest
-go install -v github.com/inspirer/textmapper/cmd/textmapper@main
-go install -v github.com/mgnsk/sway-fader@latest
-go install -v github.com/mgnsk/gh-tpl@latest
-go install -v github.com/grafana/jsonnet-language-server@latest
+go install -trimpath -v github.com/rliebz/tusk@latest
+go install -trimpath -v github.com/goccmack/gocc@latest
+go install -trimpath -v github.com/inspirer/textmapper/cmd/textmapper@main
+go install -trimpath -v github.com/mgnsk/sway-fader@latest
+go install -trimpath -v github.com/mgnsk/gh-tpl@latest
+go install -trimpath -v github.com/grafana/jsonnet-language-server@latest

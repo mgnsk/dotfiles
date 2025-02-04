07ff7b365737a09d2deab35a10f439820264a058
Magnus Kokk <magnus@kokk.eu>
2025-02-04 19:07:21 +0200

    nvim: attempt fix treesitter blink-cmp-documentation error

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -6,35 +6,36 @@ return {
 		event = "VeryLazy",
 		-- use a release tag to download pre-built binaries
 		version = "v0.11.0",
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = {
-				preset = "none",
-				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-				["<CR>"] = { "accept", "fallback" },
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			},
-			signature = { enabled = true },
-			completion = {
-				menu = {
-					auto_show = function(ctx)
-						return ctx.mode ~= "cmdline"
-					end,
+		config = function()
+			vim.treesitter.language.register("blink-cmp-documentation", "")
+
+			require("blink-cmp").setup({
+				keymap = {
+					preset = "none",
+					["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+					["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+					["<CR>"] = { "accept", "fallback" },
+					["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 				},
-				documentation = {
-					auto_show = true,
-					-- auto_show_delay_ms = 500,
+				signature = { enabled = true },
+				completion = {
+					menu = {
+						auto_show = function(ctx)
+							return ctx.mode ~= "cmdline"
+						end,
+					},
+					documentation = {
+						auto_show = true,
+						-- auto_show_delay_ms = 500,
+					},
 				},
-			},
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-		},
-		opts_extend = { "sources.default" },
+				appearance = {
+					nerd_font_variant = "mono",
+				},
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+				},
+			})
+		end,
 	},
 }

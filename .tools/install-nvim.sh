c37b75700af97ab31ea1e17dcdf0411bcdf9158b
Magnus Kokk <magnus@kokk.eu>
2025-03-10 14:09:27 +0200

    tools: fix treesitter install

--- .tools/install-nvim.sh
+++ .tools/install-nvim.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 
 nvim --headless -u NORC \
 	-c 'lua if not require("lazy_setup") then os.exit(1) end' \
-	-c 'lua local ok, result = pcall(vim.cmd, "Lazy! restore"); if not ok then print(result); os.exit(1) end' \
+	-c 'lua local ok, result = pcall(vim.cmd, "Lazy! build nvim-treesitter"); if not ok then print(result); os.exit(1) end' \
+	-c 'lua os.exit(0)'
+
+nvim --headless -u NORC \
+	-c 'lua if not require("lazy_setup") then os.exit(1) end' \
+	-c 'lua local ok, result = pcall(vim.cmd, "Lazy! build tree-sitter-balafon"); if not ok then print(result); os.exit(1) end' \
 	-c 'lua os.exit(0)'

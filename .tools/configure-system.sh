985696f94c342a8b89e3368b13b5d99abdeb2dde
Magnus Kokk <magnus@kokk.eu>
2025-03-11 20:50:03 +0200

    tools: keep tusk completion installation for now

--- .tools/configure-system.sh
+++ .tools/configure-system.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 # TODO: make this dynamic in bashrc without requiring separate file
-# install-tusk-completion() {
-# 	# Prevent tusk from modifying .bashrc.
-# 	cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
-# 	tusk --install-completion bash
-# 	mv "$HOME/.bashrc.bak" "$HOME/.bashrc"
-# }
-#
-# install-tusk-completion
+install-tusk-completion() {
+	# Prevent tusk from modifying .bashrc.
+	cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
+	tusk --install-completion bash
+	mv "$HOME/.bashrc.bak" "$HOME/.bashrc"
+}
+
+install-tusk-completion

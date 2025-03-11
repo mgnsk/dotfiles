57251c653b6db0766c3e0c55eeadefd8633dc103
Magnus Kokk <magnus@kokk.eu>
2025-03-11 23:33:26 +0200

    Stash

--- ide/build.sh
+++ ide/build.sh
@@ -2,14 +2,6 @@
 
 set -euo pipefail
 
-function get-build-context {
-	cd "$HOME" && git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" ls-files --recurse-submodules | tar Tc -
-}
-
-rm -rf dotfiles
-mkdir -p dotfiles
-get-build-context | tar Cx dotfiles
-
 docker build \
 	--build-arg uid=1000 \
 	--build-arg gid=1000 \

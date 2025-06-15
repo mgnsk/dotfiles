7693e8957fdb68bd4e6d3366ecae93470a6cf23a
Magnus Kokk <magnus@kokk.eu>
2025-06-15 21:54:41 +0300

    nvim: move back to nvim-cmp from blink.cmp nvim-cmp has been more stable

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -99,7 +99,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- 	update_in_insert = false,
 -- })
 
-local capabilities = require("blink.cmp").get_lsp_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 vim.lsp.config("*", {
 	capabilities = capabilities,

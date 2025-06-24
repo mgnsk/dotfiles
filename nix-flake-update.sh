fe9c9fa15b595bcb005b349a05af597506e3c55c
Magnus Kokk <magnus@kokk.eu>
2025-06-24 13:11:06 +0300

    nix: update dev shell packages

--- nix-flake-update.sh
+++ nix-flake-update.sh
@@ -11,13 +11,13 @@ INPUT="$1"
 FLAKE_OUTPUT=".#devShells.x86_64-linux.$2"
 
 # Build the old output and get its store path.
-OLD_STORE_PATH=$(nix build --no-link --print-out-paths "$FLAKE_OUTPUT")
+OLD_STORE_PATH=$(nix build --show-trace --no-link --print-out-paths "$FLAKE_OUTPUT")
 
 # Update flake.
 nix flake update "$INPUT"
 
 # Build the new output and get its store path.
-NEW_STORE_PATH=$(nix build --no-link --print-out-paths "$FLAKE_OUTPUT")
+NEW_STORE_PATH=$(nix build --show-trace --no-link --print-out-paths "$FLAKE_OUTPUT")
 
 # Show git diff of flake.lock.
 echo -e "\n===== flake.lock diff ====="

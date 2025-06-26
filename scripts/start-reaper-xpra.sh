f512acba89da95fae36c11e9ace4ad10e12233b7
Magnus Kokk <magnus@kokk.eu>
2025-06-26 14:06:54 +0300

    audio: improve reaper startup scripts

--- scripts/start-reaper-xpra.sh
+++ scripts/start-reaper-xpra.sh
@@ -16,8 +16,11 @@ xpra desktop :100 --resize-display="1920x1000" --start-child=openbox --exit-with
 xpra attach :100 &
 
 (
+	start=$EPOCHSECONDS
 	while ! grep -q "Starting audio shell" "$LOGFILE"; do
 		echo "0"
+		sleep 1
+		if ((EPOCHSECONDS - start > 60)); then break; fi
 	done
 	echo "100"
 ) | zenity --title="Starting Reaper..." --progress --pulsate --no-cancel --auto-close --text="Please wait..." &
--- scripts/start-reaper.sh
+++ scripts/start-reaper.sh
@@ -7,8 +7,11 @@ set -eu
 LOGFILE="$XDG_RUNTIME_DIR/reaper-$(date --iso-8601=seconds).log"
 
 (
+	start=$EPOCHSECONDS
 	while ! grep -q "Starting audio shell" "$LOGFILE"; do
 		echo "0"
+		sleep 1
+		if ((EPOCHSECONDS - start > 60)); then break; fi
 	done
 	echo "100"
 ) | zenity --title="Starting Reaper..." --progress --pulsate --no-cancel --auto-close --text="Please wait..." &

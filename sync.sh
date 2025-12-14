8998432820037c6e64e71b644a5f75234819b671
Magnus Kokk <magnus@kokk.eu>
2025-12-14 14:35:29 +0200

    Refactor sync scripts

---
+++ sync.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+set -eu
+
+declare -A mappings=(
+	["$HOME/Shared"]="gdrive:/Shared"
+	["$HOME/Studio"]="gdrive:/Studio"
+	["$HOME/.win-plugins"]="gdrive:/Audio/.win-plugins"
+	["$HOME/.vst3"]="gdrive:/Audio/.vst3"
+)
+
+for host_dir in "${!mappings[@]}"; do
+	remote_dir="${mappings[$host_dir]}"
+
+	rclone bisync \
+		--progress \
+		--copy-links \
+		--resilient \
+		--recover \
+		--max-lock 2m \
+		--conflict-resolve newer \
+		--resync-mode newer \
+		"$host_dir" \
+		"$remote_dir"
+done

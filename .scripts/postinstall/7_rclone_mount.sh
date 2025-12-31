8c295df9cecf04ac32bbb26fb5adf6fc249471ca
Magnus Kokk <magnus@kokk.eu>
2025-12-31 20:50:00 +0200

    rclone: use --vfs-cache-mode=writes

--- .scripts/postinstall/7_rclone_mount.sh
+++ .scripts/postinstall/7_rclone_mount.sh
@@ -6,6 +6,8 @@ for remote in $(rclone listremotes); do
 	remote="${remote::-1}"
 	dir="$HOME/$remote"
 	mkdir -p "$dir"
+	echo "Unmounting $remote mount at $dir if mounted"
+	systemctl --user stop "rclone@$remote"
 	echo "Enabling $remote mount at $dir"
 	systemctl --user enable --now "rclone@$remote"
 done

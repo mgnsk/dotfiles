92d05cdd045e60004c125d17c3b0b341ee4797e1
Magnus Kokk <magnus@kokk.eu>
2026-01-12 13:18:55 +0200

    rclone: mount at /mnt

--- .scripts/postinstall/7_rclone_mount.sh
+++ .scripts/postinstall/7_rclone_mount.sh
@@ -4,8 +4,9 @@ set -eu
 
 for remote in $(rclone listremotes); do
 	remote="${remote::-1}"
-	dir="$HOME/$remote"
-	mkdir -p "$dir"
+	dir="/mnt/$(id -un)/$remote"
+	sudo mkdir -p "$dir"
+	sudo chown "$(id -un):$(id -gn)" "$dir"
 	echo "Unmounting $remote mount at $dir if mounted"
 	systemctl --user stop "rclone@$remote"
 	echo "Enabling $remote mount at $dir"

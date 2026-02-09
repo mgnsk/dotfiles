43ca3ff9b2b18cae2779675e7d350c7d9d4af42a
Magnus Kokk <magnus@kokk.eu>
2026-02-09 09:37:33 +0200

    rclone: improve setup script

--- .scripts/postinstall/7_rclone_mount.sh
+++ .scripts/postinstall/7_rclone_mount.sh
@@ -2,13 +2,20 @@
 
 set -eu
 
+# TODO: before sleep, stop the services
+# when resuming, start the services again
+# to prevent broken mounts
+
 for remote in $(rclone listremotes); do
 	remote="${remote::-1}"
 	dir="/mnt/$(id -un)/$remote"
-	sudo mkdir -p "$dir"
-	sudo chown "$(id -un):$(id -gn)" "$dir"
+
 	echo "Unmounting $remote mount at $dir if mounted"
 	systemctl --user stop "rclone@$remote"
+
+	sudo mkdir -p "$dir"
+	sudo chown "$(id -un):$(id -gn)" "$dir"
+
 	echo "Enabling $remote mount at $dir"
 	systemctl --user enable --now "rclone@$remote"
 done

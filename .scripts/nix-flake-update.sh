ea0116c7a89f2ff124f9a0969d66bea10238fa13
Magnus Kokk <magnus@kokk.eu>
2026-03-30 11:22:21 +0300

    nix: improve update script

--- .scripts/nix-flake-update.sh
+++ .scripts/nix-flake-update.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-INPUT="nixpkgs"
-FLAKE_OUTPUT=".#devShells.x86_64-linux.audio"
+INPUT=$(nix flake metadata --json 2>/dev/null | gojq -r '.locks.nodes.root.inputs | keys[]' | fzf)
+FLAKE_OUTPUT=".#devShells.x86_64-linux.$(nix flake show --json 2>/dev/null | gojq -r '.devShells."x86_64-linux" | keys[]' | fzf)"
 
 # Build the old output and get its store path.
 OLD_STORE_PATH=$(nix build --show-trace --no-link --print-out-paths "$FLAKE_OUTPUT")
@@ -13,10 +13,6 @@ nix flake update "$INPUT"
 
 # Build the new output and get its store path.
 NEW_STORE_PATH=$(nix build --show-trace --no-link --print-out-paths "$FLAKE_OUTPUT")
-
-# Show git diff of flake.lock.
-echo -e "\n===== flake.lock diff ====="
-git diff flake.lock || true
 
 # Show package version changes.
 nix store diff-closures "$OLD_STORE_PATH" "$NEW_STORE_PATH"
